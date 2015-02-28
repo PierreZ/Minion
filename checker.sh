e0e8475586315383a268522ff7f722a78cdf4237
PierreZ <pierre.zemb.isen@gmail.com>
2015-02-28 09:45:03 -0500

    adding new features

--- checker.sh
+++ checker.sh
@@ -7,16 +7,18 @@
 Service_name=(sshd nginx);
 
 # Declaration of services running into containers
-Container_name=(shout ghost);
+Container_name=(shout ghost elementz);
 
 # Declaration of git
-git_repo=('/srv/http/sw' '/srv/http/portfolio');
+git_repo=('/srv/http/slides/docker-presentation-gdg-2014' '/srv/http/portfolio');
 
 # Definition of the colors
 neutre='\e[0;m'
 rougefonce='\e[0;31m'
 vertclair='\e[1;32m'
 
+echo;
+echo "There's $(checkupdates|wc -l) update(s) available"
 echo;
 echo "Is everything OK boss ?"
 echo;
@@ -29,7 +31,9 @@ for i in "${Service_name[@]}"; do
 		printf "%-20s [${rougefonce}not running${neutre}]\n" "$i";
 	fi
 done
-
+echo;
+echo "What about docker?";
+echo;
 # Start looping for docker's container
 for i in "${Container_name[@]}"; do
 	docker ps | grep $i  > /dev/null
@@ -47,6 +51,8 @@ for i in "${Container_name[@]}"; do
 	fi
 done
 
+echo;
+echo "Updating GitHub repos";
 for repo in "${git_repo[@]}"; do
     echo "${repo}"
     (cd "${repo}" && git pull)
